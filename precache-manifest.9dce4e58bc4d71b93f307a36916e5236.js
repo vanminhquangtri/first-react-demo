425c778938703fce2b46cc6795ff958c3baa50c5
Van Minh <vanminhquangtri@gmail.com>
2020-10-17 10:41:52 +0700

    Updates

---
+++ precache-manifest.9dce4e58bc4d71b93f307a36916e5236.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7dd7cf83fd994f7f0c67f6ad140f396",
+    "url": "/first-react-demo/index.html"
+  },
+  {
+    "revision": "7bdc1bc1bcdd58437f31",
+    "url": "/first-react-demo/static/css/2.c00955b9.chunk.css"
+  },
+  {
+    "revision": "9517a54fd0d883ebb885",
+    "url": "/first-react-demo/static/css/main.d79f5e21.chunk.css"
+  },
+  {
+    "revision": "7bdc1bc1bcdd58437f31",
+    "url": "/first-react-demo/static/js/2.191aa6f1.chunk.js"
+  },
+  {
+    "revision": "a23aa5ad74bbccef876e3906f8c6f1a1",
+    "url": "/first-react-demo/static/js/2.191aa6f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9517a54fd0d883ebb885",
+    "url": "/first-react-demo/static/js/main.45b9190d.chunk.js"
+  },
+  {
+    "revision": "24829f3eddf8fbc637b6",
+    "url": "/first-react-demo/static/js/runtime-main.243d261b.js"
+  },
+  {
+    "revision": "a7ecc9e38db17cc2ffffe6680eb1afe2",
+    "url": "/first-react-demo/static/media/david-veksler-VW5YwCYbPyk-unsplash.a7ecc9e3.jpg"
+  },
+  {
+    "revision": "c14d841d5644c0785c6fdc237da0d961",
+    "url": "/first-react-demo/static/media/douglas-sheppard-9rYfG8sWRVo-unsplash-1024x683.c14d841d.jpg"
+  },
+  {
+    "revision": "ef9c9d62926f4c9cf6e025e03ecbffe6",
+    "url": "/first-react-demo/static/media/family.ef9c9d62.svg"
+  },
+  {
+    "revision": "898bddbd5b6f034b512a3b192c674f47",
+    "url": "/first-react-demo/static/media/florian-schmidinger-b_79nOqf95I-unsplash.898bddbd.jpg"
+  },
+  {
+    "revision": "ee768985d7ccd3ff7763d6a56ccd7658",
+    "url": "/first-react-demo/static/media/francesca-tosolini-DmOhItSo49k-unsplash-1024x683.ee768985.jpg"
+  },
+  {
+    "revision": "50ba55ffb120576163b4c355fbec7e1f",
+    "url": "/first-react-demo/static/media/francesca-tosolini-Mx_W2atEaDo-unsplash-1024x683.50ba55ff.jpg"
+  },
+  {
+    "revision": "53a1a3cb3278ed4df57809b043e311ee",
+    "url": "/first-react-demo/static/media/francesca-tosolini-l43rLPXCP1M-unsplash-1024x683.53a1a3cb.jpg"
+  },
+  {
+    "revision": "345228adaa3061ef38fca1a657922035",
+    "url": "/first-react-demo/static/media/im3rd-media-CbZ4EDP__VQ-unsplash-1024x683.345228ad.jpg"
+  },
+  {
+    "revision": "2f6d71953ecfb74af9adad8a61e55360",
+    "url": "/first-react-demo/static/media/insurance.2f6d7195.svg"
+  },
+  {
+    "revision": "6f9b8bb8854fa977381a6f620f03f86b",
+    "url": "/first-react-demo/static/media/jacques-bopp-Hh18POSx5qk-unsplash-1.6f9b8bb8.jpg"
+  },
+  {
+    "revision": "f799c02574c784bcdfde7da287a0dba6",
+    "url": "/first-react-demo/static/media/joshua-hanks-MaMC5zV7uhs-unsplash-1024x683.f799c025.jpg"
+  },
+  {
+    "revision": "362fd85480ecd4cbf9ba1eec630c787a",
+    "url": "/first-react-demo/static/media/joss-woodhead-3wFRlwS91yk-unsplash-1024x683.362fd854.jpg"
+  },
+  {
+    "revision": "09b245c3e65a4c91e2cf202e78422246",
+    "url": "/first-react-demo/static/media/kara-eads-L7EwHkq1B2s-unsplash-1024x683.09b245c3.jpg"
+  },
+  {
+    "revision": "dc437c5ccfeeedfb63e020946b042197",
+    "url": "/first-react-demo/static/media/maximillian-conacher-sPpe2D7VbpM-unsplash-1024x768.dc437c5c.jpg"
+  },
+  {
+    "revision": "643ed352700b07d7ce874c4fffcdd425",
+    "url": "/first-react-demo/static/media/morning-brew-SQ5Lx-pCvDI-unsplash-1024x683.643ed352.jpg"
+  },
+  {
+    "revision": "00006f6379204fcf85bd400b025a63e9",
+    "url": "/first-react-demo/static/media/naomi-ellsworth-EMPLSuvDuhQ-unsplash.00006f63.jpg"
+  },
+  {
+    "revision": "b0290be28da1394bf3fc3e3ed091eaa7",
+    "url": "/first-react-demo/static/media/next-button(svg).b0290be2.svg"
+  },
+  {
+    "revision": "dcecc703d0f2d8b522c3eefef12c6ba6",
+    "url": "/first-react-demo/static/media/previous(svg).dcecc703.svg"
+  },
+  {
+    "revision": "3a0464892aea367a55c886e1c76bfdb5",
+    "url": "/first-react-demo/static/media/ralph-kelly-z9fFOzL5L_Y-unsplash.3a046489.jpg"
+  },
+  {
+    "revision": "165e75af6e19283ddf95443fe68ea9a5",
+    "url": "/first-react-demo/static/media/stephan-bechert-yFV39g6AZ5o-unsplash.165e75af.jpg"
+  },
+  {
+    "revision": "a07fe96811a4c3c378c769a6bd8f933c",
+    "url": "/first-react-demo/static/media/stephen-leonardi-8hbbbCN1ds8-unsplash.a07fe968.jpg"
+  },
+  {
+    "revision": "f150645212b687e672cfd0a3162fcf94",
+    "url": "/first-react-demo/static/media/stephen-leonardi-Al9Cl-b7EFU-unsplash.f1506452.jpg"
+  },
+  {
+    "revision": "e377ff7e7b839a6fb3eb9447f359d834",
+    "url": "/first-react-demo/static/media/steven-shaffer-GVpzXI4bkUI-unsplash-1024x768.e377ff7e.jpg"
+  },
+  {
+    "revision": "ab8763feaa86e3aa1ed1bbc06861a5b6",
+    "url": "/first-react-demo/static/media/team.ab8763fe.svg"
+  },
+  {
+    "revision": "456cd003425b7cd38303b738991dd035",
+    "url": "/first-react-demo/static/media/up-chevron.456cd003.svg"
+  },
+  {
+    "revision": "a0766e9577206f1f0edcc9ac389b7c4d",
+    "url": "/first-react-demo/static/media/vu-anh-TiVPTYCG_3E-unsplash.a0766e95.jpg"
+  }
+]);
--- src/Components/GeneralModules/ScrollToTop.js
+++
@@ -1,7 +0,0 @@
-import $ from "jquery";
-const ScrollToTop = function (position, speed){
-    return $('html, body').animate ({
-        scrollTop: position
-    }, speed)
-}
-export default ScrollToTop;
